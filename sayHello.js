13cc7c0cdc0ae352a681a14ac1a7512be697caac
Hasegawa-Akito <h.akito1999@gmail.com>
2022-12-26 22:32:30 +0900

    say Hello

---
+++ sayHello.js
@@ -0,0 +1,5 @@
+var sayHello = function (firstName) {
+    console.log("sayHello" + firstName);
+};
+var firstName = "Akito";
+sayHello(firstName);
---
+++ sayHello.ts
@@ -0,0 +1,6 @@
+const sayHello = (firstName: string) => {
+  console.log("sayHello" + firstName);
+};
+
+let firstName: string = "Akito";
+sayHello(firstName);
