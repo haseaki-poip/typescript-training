5f918a3eb757ae60400647289470e84f3719952d
Hasegawa-Akito <h.akito1999@gmail.com>
2023-03-05 22:16:53 +0900

    conditional

--- Conditional.ts
+++ Conditional.ts
@@ -1,6 +1,6 @@
 // Conditional Types
 // type MyCondition<T, U, X, Y> = T extends U ? X : Y;
-// TがUに代入可能であればXを、そうでなければY
+// 型Tが型Uを満たす場合はXを、そうでなければY
 
 // Union typesの分配則: Union typesのConditional Typesは、それぞれのConditional TypesのUnionに展開される。
 // すなわち、 (T1 | T2) extends U ? X : Y = (T1 extends U ? X : Y) | (T2 extends U ? X : Y)
@@ -8,3 +8,6 @@
 type Diff<T, U> = T extends U ? never : T;
 type Result = Diff<"hoge" | "foo" | "piyo", "foo">;
 // Resultは"hoge" | never | "piyo" つまり"hoge" | "piyo"の型となる
+
+type Any<T> = T extends any ? T : any;
+type Hoge = Any<string | number>;
